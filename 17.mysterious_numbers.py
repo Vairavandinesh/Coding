3369586b1976e18d4645bd0b66d8e1f0f1d6f0a6
Vairavandinesh <140646315+Vairavandinesh@users.noreply.github.com>
2025-01-26 18:50:45 +0530

    Create 17.mysterious_numbers.py

---
+++ 17.mysterious_numbers.py
@@ -0,0 +1,8 @@
+a=int(input("Enter number 1:"))
+b=int(input("Enter number 2:"))
+if a>b:
+    print("a is greater than b")
+elif a<b:
+    print("b is greater than a")
+elif a==b:
+    print("Both numbers are equal")
