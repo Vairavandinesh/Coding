32c30dab0933fd8974744c55d1395bae8067eeb2
Vairavandinesh <140646315+Vairavandinesh@users.noreply.github.com>
2025-01-26 18:54:40 +0530

    Create 18.comparisons.py

---
+++ 18.comparisons.py
@@ -0,0 +1,12 @@
+a=int(input("Enter a number:"))
+b=int(input("Enter another number:"))
+if a>=b:
+    print(a,'>=',b)
+elif a<b:
+    print(a,'<',b)
+elif a<=b:
+    print(a,'<=',b)
+elif a==b:
+    print(a,'==',b)
+elif a!=b:
+    print(a,'!=',b)
